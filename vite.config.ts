61c03cf148a1e6c068bc66abe3140b624002a335
bakar301 <abubakarazeem32@gmail.com>
2025-07-10 20:50:53 +0500

    Updated url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+
 export default defineConfig({
+  base: '/abubakar-portfolio/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
